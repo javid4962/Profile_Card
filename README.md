fbe225e20bb534e199e016976374fdb8815fdd1c
javid4962 <shaikmasthanjavidvali@gmail.com>
2024-03-07 09:31:25 +0530

    Profile Card Created

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Profile Card
+## Skills set used
+- HTML
+- CSS
+- JavaScript
+- Editor - vscode
+
+## Images
+![alt text](img/image1.png)
+
+![alt text](img/image2.png)
---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#one', {
+    strings: ['Masthan Javid Vali Shaik'],
+    typeSpeed: 50,
+    loop:true,
+    showCursor:false,
+    // animation: blink 0.7s infinite;
+});
+// var typed = new Typed('#two', {
+//     strings: ['Fontend Web Development'],
+//     typeSpeed: 50,
+//     loop:true,
+//     showCursor:false,
+//   });
